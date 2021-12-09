9d9c364451684c40a63bbe08fd5e0f6c2a0f929f
Anna H <70937358+Haruberi@users.noreply.github.com>
2021-12-09 11:49:12 +0100

    init

---
+++ MathematicalOperations.cs
@@ -0,0 +1,154 @@
+﻿using System;
+namespace Calculator
+{
+    public class MathematicalOperations
+    {
+        public double numberOne;
+        public string numberOneString = "";
+        public string numberTwoString = "";
+        public double numberTwo;
+        public double result;
+        public bool succeed = false;
+
+        public MathematicalOperations()
+        {
+        }
+
+        /* This method will be executed when the user types + 
+         * There will be a check to see if the inserted value is invalid.
+         * A string will be executed to tell the user if the input is invalid. 
+         * Lastly, when two correct numbers has been inserted, 
+           the result of the two numbers will be calculated and executed.*/
+
+        public void Add()
+        {
+            Console.Clear();
+            Console.WriteLine("\t+ + Addition + +\n");
+            Console.WriteLine("Type a number: ");
+
+            var numberOneString = Console.ReadLine();
+
+            while (!double.TryParse(numberOneString, out numberOne))
+            {
+                Console.WriteLine("Invalid number. Please enter a valid number:");
+                numberOneString = Console.ReadLine();
+            }
+
+            Console.WriteLine("Type another number, then press enter: ");
+            var numberTwoString = Console.ReadLine();
+
+            while (!double.TryParse(numberTwoString, out numberTwo))
+            {
+                Console.WriteLine("Invalid number. Please enter a valid number: ");
+                numberTwoString = Console.ReadLine();
+            }
+            result = numberOne + numberTwo;
+            Console.WriteLine("{0} + {1} = {2}", numberOne, numberTwo, result);
+        }
+
+
+        /* This method will be executed when the user types - 
+        *  There will be a check to see if the inserted value is invalid.
+        *  A string will be executed to tell the user if the input is invalid. 
+         * Lastly, when two correct numbers has been inserted, 
+         * the result of the two numbers will be calculated and executed.*/
+
+        public void Subtract()
+        {
+            Console.Clear();
+            Console.WriteLine("\t- - Subtraction - -\n");
+            Console.WriteLine("Type a number: ");
+
+            var numberOneString = Console.ReadLine();
+            while (!double.TryParse(numberOneString, out numberOne))
+            {
+                Console.WriteLine("Invalid number. Please enter a valid number:");
+                numberOneString = Console.ReadLine();
+            }
+
+            Console.WriteLine("Type another number, then press enter: ");
+            var numberTwoString = Console.ReadLine();
+            while (!double.TryParse(numberTwoString, out numberTwo))
+            {
+                Console.WriteLine("Invalid number. Please enter a valid number: ");
+                numberTwoString = Console.ReadLine();
+            }
+
+            result = numberOne - numberTwo;
+            Console.WriteLine("{0} - {1} = {2}", numberOne, numberTwo, result);
+        }
+
+        /* This method will be executed when the user types x 
+         * There will be a check to see if the inserted value is invalid.
+         * A string will be executed to tell the user if the input is invalid. 
+         * Lastly, when two correct numbers has been inserted, 
+           the result of the two numbers will be calculated and executed.*/
+
+        public void Multiplicate()
+        {
+            Console.Clear();
+            Console.WriteLine("\tx x Multiplication x x\n");
+            Console.WriteLine("Type a number: ");
+
+            var numberOneString = Console.ReadLine();
+            while (!double.TryParse(numberOneString, out numberOne))
+            {
+                Console.WriteLine("Invalid number. Please enter a valid number:");
+                numberOneString = Console.ReadLine();
+            }
+
+            Console.WriteLine("Type another number, then press enter: ");
+            var numberTwoString = Console.ReadLine();
+            while (!double.TryParse(numberTwoString, out numberTwo))
+            {
+                Console.WriteLine("Invalid number. Please enter a valid number: ");
+                numberTwoString = Console.ReadLine();
+            }
+
+            result = numberOne * numberTwo;
+            Console.WriteLine("{0} x {1} = {2}", numberOne, numberTwo, result);
+        }
+
+        /* This method will be executed when the user types / 
+         * There will be a check to see if the inserted value is 0 or if it is invalid.
+         * A string will be executed to tell the user if the input is invalid. 
+         * Lastly, when two correct numbers has been inserted, 
+           the result of the two numbers will be calculated and executed.*/
+
+        public void Divide()
+        {
+            Console.Clear();
+            Console.WriteLine("\t/ / Division / /\n");
+            Console.WriteLine("Type a number: ");
+
+            var numberOneString = Console.ReadLine();
+            while (!double.TryParse(numberOneString, out numberOne) || numberOneString == "0")
+            {
+                Console.WriteLine("Invalid number. Please enter a valid number:");
+                numberOneString = Console.ReadLine();
+            }
+
+            Console.WriteLine("Type another number, then press enter: ");
+            var numberTwoString = Console.ReadLine();
+            while (!double.TryParse(numberTwoString, out numberTwo) || numberTwoString == "0")
+            {
+                Console.WriteLine("Invalid number. Please enter a valid number: ");
+                numberTwoString = Console.ReadLine();
+            }
+
+            result = numberOne / numberTwo;
+            Console.WriteLine("{0} / {1} = {2}", numberOne, numberTwo, result);
+        }
+
+        /*This method will be executed when the user types e 
+          This method will close the Calculator application.*/
+
+        public void ExitCalculator()
+        {
+            Console.Clear();
+            Console.WriteLine("\t Press\n\t ENTER\n to close the calculator.");
+            Console.ReadLine();
+            Environment.Exit(1);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mathOp = new MathematicalOperations();
+
+            string mathOptions;
+            bool running = true;
+            while (running)
+            {
+                //Menu system for mathematical operations and an Exit option
+                Console.Clear();
+                Console.WriteLine("  Choose an option within the parantheses:\n ");
+                Console.WriteLine("\t|-----------------------|");
+                Console.WriteLine("\t| (+) Addition          |");
+                Console.WriteLine("\t|-----------------------|");
+                Console.WriteLine("\t| (-) Subtraction       |");
+                Console.WriteLine("\t|-----------------------|");
+                Console.WriteLine("\t| (x) Multiplication    |");
+                Console.WriteLine("\t|-----------------------|");
+                Console.WriteLine("\t| (/) Division          |");
+                Console.WriteLine("\t|-----------------------|");
+                Console.WriteLine("\t| (e) Exit              |");
+                Console.WriteLine("\t|-----------------------|");
+
+                mathOptions = Console.ReadLine();
+                switch (mathOptions)
+                {
+                    //Each method is located at class MathematicalOperations.cs
+                    case "+":
+                        mathOp.Add();
+                        break;
+                    case "-":
+                        mathOp.Subtract();
+                        break;
+                    case "x":
+                        mathOp.Multiplicate();
+                        break;
+                    case "/":
+                        mathOp.Divide();
+                        break;
+                    case "e":
+                        mathOp.ExitCalculator();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
+
+
